2bde50ebcdcccfac3a1b9481a1c8a68c33d8c112
igor394 <svidunovich-igor@rambler.ru>
2021-04-16 13:31:14 +0300

    connection database

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
 const router = require('./routes/index')
+const sequelize = require('./modelDb/db')
+const models = require('./modelDb/models')
 
 
 const PORT = process.env.PORT || 3303
@@ -11,4 +13,14 @@ app.use(express.json())
 app.use('/api', router)
 
 
-app.listen(PORT, () => console.log(`start serer ${PORT}`))
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`start serer ${PORT}`))
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+start()
