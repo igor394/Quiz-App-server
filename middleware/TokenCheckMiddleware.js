e898047d91084313f750b8ec62132890b44ca12a
igor394 <svidunovich-igor@rambler.ru>
2021-04-16 14:29:27 +0300

    registration authorization on the server completed with saving in the database

---
+++ middleware/TokenCheckMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = function (req, res, next) {
+    if (req.method === 'OPTIONS') {
+        next()
+    }
+    try {
+        const token = req.headers.authorization.split(' ')[1] //first comes the name then the token itself
+        if (!token) {
+            return res.status(401).json({ message: 'not authorization' })
+        }
+        const decoder = jwt.verify(token, process.env.SECRET_KEY)
+        req.user = decoder
+        next()
+    } catch (e) {
+        res.status(401).json({ message: 'not authorization' })
+    }
+}
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,12 @@
 const Router = require('express')
 const userControllers = require('../controllers/userControllers')
 const router = new Router()
+const TokenCheckMiddleware = require('../middleware/TokenCheckMiddleware')
 
 
 
 router.post('/reg', userControllers.registr)
 router.post('/login', userControllers.login)
-router.get('/auth', userControllers.check)
+router.get('/auth',TokenCheckMiddleware, userControllers.check)
 
 module.exports = router;
